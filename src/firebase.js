45d9860f8a32f118eb98541fed230d7fd763d301
ajgeorge <aaronjosgeorge@gmail.com>
2025-12-07 18:36:38 +0000

    Update Firebase configuration and add security rules

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCSqeWyyDGX8EZcNmrBTb92_c93TA0jnik',
-  authDomain: 'call-track-164b6.firebaseapp.com',
-  projectId: 'call-track-164b6',
-  storageBucket: 'call-track-164b6.firebasestorage.app',
-  messagingSenderId: '865867377848',
-  appId: '1:865867377848:web:21fb506b37a39b774e984d',
+  apiKey: "AIzaSyC4drxf0WZZWDhk9wP2yWB077XibEIXct8",
+  authDomain: "call-track-d1699.firebaseapp.com",
+  projectId: "call-track-d1699",
+  storageBucket: "call-track-d1699.firebasestorage.app",
+  messagingSenderId: "483015685810",
+  appId: "1:483015685810:web:7ea3e4938cc8e8f5f3fbab"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
